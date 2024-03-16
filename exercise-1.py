27b2b557dd34679b960cce3b88014397009103f7
Ung-Ouchy <156298357+Ung-Ouchy@users.noreply.github.com>
2024-03-16 23:39:47 +0700

    Update exercise-1.py

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,8 @@
 def replace_last(numbers):
-    ...
+    if len(numbers)<2:
+        return numbers
+    else:
+        else:
+        numbers.insert(0, numbers[-1])
+        numbers.pop()
+        return numbers
