458449eb7efe84eb4835f8dc103c2102ee9991ed
dy-nobsokun1 <157293661+dy-nobsokun1@users.noreply.github.com>
2024-03-17 18:30:15 +0700

    Update exercise-3.py

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,17 @@
 def remove_all_after(numbers, n):
-    ...
+    size = len(numbers)
+
+    if size == 0:
+        return numbers
+    
+    else: 
+
+        if n in numbers:
+            result = []
+            ind = numbers.index(n)
+            result = numbers[:ind]
+            return result 
+
+        else: 
+            return numbers
+   
